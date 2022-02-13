4f18cb12b309e79967f461e575378b504e1bfda3
cconcann <97709188+cconcann@users.noreply.github.com>
2022-02-13 00:53:42 -0500

    9

--- itis3135/scripts/defaultscript.js
+++ itis3135/scripts/defaultscript.js
@@ -8,3 +8,12 @@ function testFunction()
 {
     alert("this is a test");
 }
+
+function displayDatetime() 
+{
+    let today = new Date();
+    let month = today.getMonth();
+    let monthWord = new Array('January', 'February', 'March', 'April', 'May', 'June', 
+    'July', 'August', 'September', 'October', 'November', 'December');
+    document.getElementById('datetime').innerHTML = "The current time is: " + today.getHours() + ":" + today.getMinutes() + ".  Todays date is: " + monthWord[month] + " " + today.getDate() + ", " + today.getFullYear() + ".";
+}
