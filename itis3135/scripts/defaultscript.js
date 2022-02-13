b6744c5ea5d1336477667fd24d38a08be61d0acf
cconcann <97709188+cconcann@users.noreply.github.com>
2022-02-13 01:19:29 -0500

    12

--- itis3135/scripts/defaultscript.js
+++ itis3135/scripts/defaultscript.js
@@ -11,3 +11,10 @@ function displayDateTime()
     'July', 'August', 'September', 'October', 'November', 'December');
     document.getElementById('datetime').innerHTML = "The current time is: " + today.getHours() + ":" + today.getMinutes() + ".  Todays date is: " + monthWord[month] + " " + today.getDate() + ", " + today.getFullYear() + ".";
 }
+
+function greeting()
+{
+    let userName = document.getElementById('firstname').value;
+    let userFeeling = document.getElementById('feeling').value;
+    document.getElementById("output").innerHTML = "Welcome, " + userName + "!  Thankyou for telling us you are feeling " + userFeeling + ".";
+}
