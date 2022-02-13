a60d5ce16d304c4e19ceb44561c7c8eb85397b2d
cconcann <97709188+cconcann@users.noreply.github.com>
2022-02-13 00:42:15 -0500

    test6

--- itis3135/scripts/defaultscript.js
+++ itis3135/scripts/defaultscript.js
@@ -2,13 +2,17 @@
 
 
 
-var today = new Date();
-var day = today.getDay();
-var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time; 
-document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+dateTime();
+
+function dateTime() {
+    var today = new Date();
+    var day = today.getDay();
+    var daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+}
 
 function testFunction()
 {
