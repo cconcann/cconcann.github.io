3f2b7392622651da5e5be330ec0ee73af939020f
cconcann <97709188+cconcann@users.noreply.github.com>
2022-02-27 22:09:45 -0500

    3 more buttons

--- itis3135/scripts/calculator.js
+++ itis3135/scripts/calculator.js
@@ -6,3 +6,30 @@ function additionFunction()
     const calcvalue = first_number + second_number;
     alert("The sum of your two numbers is " + calcvalue);
 }
+
+function subtractionFunction()
+{
+    let first_number = parseInt(document.getElementById('firstnumber').value);
+    let second_number = parseInt(document.getElementById('secondnumber').value);
+
+    const calcvalue = first_number - second_number;
+    alert("The difference of your two numbers is " + calcvalue);
+}
+
+function multiplicationFunction()
+{
+    let first_number = parseInt(document.getElementById('firstnumber').value);
+    let second_number = parseInt(document.getElementById('secondnumber').value);
+
+    const calcvalue = first_number * second_number;
+    alert("The total of your two numbers is " + calcvalue);
+}
+
+function divisionFunction()
+{
+    let first_number = parseInt(document.getElementById('firstnumber').value);
+    let second_number = parseInt(document.getElementById('secondnumber').value);
+
+    const calcvalue = first_number / second_number;
+    alert("The remainder of your two numbers is " + calcvalue);
+}
