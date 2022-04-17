0139ab35578a9eb2598f179724dce064ed27c47f
cconcann <97709188+cconcann@users.noreply.github.com>
2022-04-17 17:15:07 -0400

    adjusted

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,12 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -16,14 +14,22 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  if (n > slides.length) 
+    {
+      slideIndex = 1
+    }
+  if (n < 1) 
+    {
+        slideIndex = slides.length
+    }
+  for (i = 0; i < slides.length; i++) 
+    {
+        slides[i].style.display = "none";
+    }
+  for (i = 0; i < dots.length; i++) 
+    {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
