d45ed8999024dd01fff319dbfac6f06a1b38b512
cconcann <97709188+cconcann@users.noreply.github.com>
2022-04-18 16:45:27 -0400

    fixing swap

--- itis3135/scripts/Activity10.js
+++ itis3135/scripts/Activity10.js
@@ -10,10 +10,13 @@ $(document).ready(function() {
         $("#image_list a").click( evt => {
                 const link = evt.currentTarget;
 
-                $("#main_image").attr("src", link.href);
+                $("#main_image").fadeOut(1000, function() {
+                $("#main_image").attr("src", link.href).fadeIn(1000);
+                });
 
-                $("#caption").text(link.title);
-
+                $("#caption").fadeOut(1000,function() {
+                $("#caption").text(link.title).fadeIn(1000);
+                });
                 evt.preventDefault();
         });
     			// get the image URL and caption for each image and animate the caption
