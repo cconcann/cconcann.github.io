90e8158a2a3ae6f8dae8115a8374db217a054f00
cconcann <97709188+cconcann@users.noreply.github.com>
2022-04-20 16:13:55 -0400

    added  act 12

---
+++ itis3135/Activity12.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 300,
+        slideMargin: 20,
+        pager: true,
+        speed: 4000,
+        pagerType: 'short',
+        captions: true,
+        pagerSelector: '#id_paper'
+
+    });
+});
